51226cf4d241996d1c2c69ecb9dc806d652289ef
Angelito Sardez, Jr <jojosardez@gmail.com>
2019-02-11 22:49:26 +1100

    Dataset preparation and stuff.

---
+++ reviewDataset.php
@@ -0,0 +1,12 @@
+<?php
+namespace PhpmlExercise;
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Phpml\Dataset\CsvDataset;
+
+$dataset = new CsvDataset('datasets/clean_tweets.csv',1);
+
+foreach ($dataset->getSamples() as $sample) {
+    print_r($sample);
+}
